e0748d6554f9df11947f5800cec16ae91998a1aa
Aryan <aryan.saxena094@gmail.com>
2023-03-20 02:42:45 -0400

    fix error

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ num1 = Math.floor((Math.random()*6)+1);
 num2 = Math.floor((Math.random()*6)+1);
 dicenum1 = "images/dice" + num1 + ".png";
 dicenum2 = "images/dice" + num2 + ".png";
-
 document.querySelectorAll("img")[0].setAttribute("src",dicenum1);
 document.querySelectorAll("img")[1].setAttribute("src",dicenum2);
-
 if(num1>num2){
     document.querySelector("h1").innerText ="Player 1 wins 🚩";
 }
-else if(num2<num1){
+else if(num2>num1){
     document.querySelector("h1").innerText = "🚩 Player 2 wins";
 }
 else{
